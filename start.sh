0f4fd19a6bad0319da7a7f882c385ab9ad9984c7
PauloSantangelo <paulocbsantangelo@gmail.com>
2024-07-04 09:10:52 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-chmod +x start.sh
+#!/bin/bash
+
+# Navegue até o diretório correto se necessário
+# cd main  # Descomente se necessário e substitua 'main' pelo diretório correto
+
+# Inicie o aplicativo Flask com gunicorn
+exec gunicorn --bind 0.0.0.0:5000 app:app
